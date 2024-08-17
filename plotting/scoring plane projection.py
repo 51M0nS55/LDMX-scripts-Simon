7122b1c946dcb41e51e44e94ee418a0696717b9a
Simon S <157428226+51M0nS55@users.noreply.github.com>
2024-08-16 22:53:47 -0700

    Update scoring plane projection.py

--- plotting/scoring plane projection.py
+++ plotting/scoring plane projection.py
@@ -1,72 +1,190 @@
 import numpy as np
-import awkward as ak
 import uproot
+import glob
+import re
+import math
+from tqdm import tqdm
+import awkward
+import concurrent.futures
+import json
 
-# File path to analyze
-file_path = '/home/vamitamas/Samples8GeV/Ap0.001GeV_sim/*.root'
+executor = concurrent.futures.ThreadPoolExecutor(20)
 
-# Branches we're interested in
-branchList = [
-    'TargetScoringPlaneHits_signal.x_',
-    'TargetScoringPlaneHits_signal.y_',
-    'TargetScoringPlaneHits_signal.z_',
-    'TargetScoringPlaneHits_signal.energy_'
-]
+# detector constants
+SP_TARGET_DOWN_Z = 0.1767
+ECAL_SP_Z = 239.9985
+ECAL_FACE_Z = 247.932
+CELL_RADIUS = 5.0
 
-# Parameters for fiducial cuts (e.g., these values should reflect your experiment)
-fiducial_cuts = {
-    "x_min": -50, "x_max": 50,
-    "y_min": -50, "y_max": 50,
-    "z_min": 100, "z_max": 200
+# Functions for distance and projection calculations
+def dist(p1, p2):
+    return math.sqrt(np.sum((np.array(p1) - np.array(p2)) ** 2))
+
+def projection(Recoilx, Recoily, Recoilz, RPx, RPy, RPz, HitZ):
+    x_final = Recoilx + RPx / RPz * (HitZ - Recoilz) if RPz else 0
+    y_final = Recoily + RPy / RPz * (HitZ - Recoilz) if RPy else 0
+    return (x_final, y_final)
+
+def _load_cellMap(version='v13'):
+    cellMap = {}
+    for i, x, y in np.loadtxt(f'/home/duncansw/LDMX-scripts/GraphNet/data/{version}/cellmodule.txt'):
+        cellMap[i] = (x, y)
+    global cells
+    cells = np.array(list(cellMap.values()))
+    print("Loaded {} detector info".format(version))
+
+def pad_array(arr):
+    arr = awkward.pad_none(arr, 1, clip=True)
+    arr = awkward.fill_none(arr, 0)
+    return awkward.flatten(arr)
+
+# v14 8gev files
+file_templates = {
+    0.001: '/home/vamitamas/Samples8GeV/Ap0.001GeV_sim/*.root',
+    0.01: '/home/vamitamas/Samples8GeV/Ap0.01GeV_sim/*.root',
+    0.1: '/home/vamitamas/Samples8GeV/Ap0.1GeV_sim/*.root',
+    1.0: '/home/vamitamas/Samples8GeV/Ap1GeV_sim/*.root',
+    0: '/home/vamitamas/Samples8GeV/v3.3.3_ecalPN*/*.root'
 }
 
-def is_non_fiducial(x, y, z):
-    return (x < fiducial_cuts["x_min"]) | (x > fiducial_cuts["x_max"]) | \
-           (y < fiducial_cuts["y_min"]) | (y > fiducial_cuts["y_max"]) | \
-           (z < fiducial_cuts["z_min"]) | (z > fiducial_cuts["z_max"])
+# Load ecal cell geometry
+_load_cellMap()
 
-def analyze_file(file_name):
-    with uproot.open(file_name) as file:
-        tree = file['LDMX_Events;6']
-        data = tree.arrays(branchList, interpretation_executor=None)
-        
-        x = data[branchList[0]]
-        y = data[branchList[1]]
-        z = data[branchList[2]]
-        energy = data[branchList[3]]
+# Dictionary for nonfiducial ratios (for each mass point)
+nonfid_ratios = {}
+# Loop over mass points
+for mass in file_templates.keys():
 
-        # Find non-fiducial events
-        non_fiducial_mask = is_non_fiducial(x, y, z)
-        non_fiducial_energy = energy[non_fiducial_mask]
-        
-        return len(non_fiducial_energy), np.sum(non_fiducial_energy), len(energy)
+    print(f"==== m = {mass} ====", flush=True)
 
-def compute_non_fiducial_ratio(file_path):
-    files = uproot.iterate(f"{file_path}:LDMX_Events;6", expressions=branchList)
-    
-    total_events = 0
-    non_fiducial_events = 0
-    non_fiducial_energy_total = 0
-    
-    for file_name, arrays in files:
-        n_non_fiducial, non_fid_energy, n_events = analyze_file(file_name)
-        
-        total_events += n_events
-        non_fiducial_events += n_non_fiducial
-        non_fiducial_energy_total += non_fid_energy
+    # Branch lists for signal vs. background
+    if mass:
+        branchList = ['TargetScoringPlaneHits_signal/TargetScoringPlaneHits_signal.pdgID_', 'TargetScoringPlaneHits_signal/TargetScoringPlaneHits_signal.x_',
+                      'TargetScoringPlaneHits_signal/TargetScoringPlaneHits_signal.y_', 'TargetScoringPlaneHits_signal/TargetScoringPlaneHits_signal.z_',
+                      'TargetScoringPlaneHits_signal/TargetScoringPlaneHits_signal.px_', 'TargetScoringPlaneHits_signal/TargetScoringPlaneHits_signal.py_',
+                      'TargetScoringPlaneHits_signal/TargetScoringPlaneHits_signal.pz_', 'TriggerSums20Layers_signal/pass_',
+                      'EcalRecHits_signal/EcalRecHits_signal.energy_', 'EcalRecHits_signal/EcalRecHits_signal.isNoise_']
+    else:
+        branchList = ['TargetScoringPlaneHits_sim/TargetScoringPlaneHits_sim.pdgID_', 'TargetScoringPlaneHits_sim/TargetScoringPlaneHits_sim.x_',
+                      'TargetScoringPlaneHits_sim/TargetScoringPlaneHits_sim.y_', 'TargetScoringPlaneHits_sim/TargetScoringPlaneHits_sim.z_',
+                      'TargetScoringPlaneHits_sim/TargetScoringPlaneHits_sim.px_', 'TargetScoringPlaneHits_sim/TargetScoringPlaneHits_sim.py_',
+                      'TargetScoringPlaneHits_sim/TargetScoringPlaneHits_sim.pz_', 'EcalRecHits_sim/EcalRecHits_sim.energy_',
+                      'EcalRecHits_sim/EcalRecHits_sim.isNoise_']
 
-    if total_events == 0:
-        return None
+    file_list = glob.glob(file_templates[mass])
+    nFiles = len(file_list)
 
-    ratio = non_fiducial_events / total_events
-    uncertainty = np.sqrt(ratio * (1 - ratio) / total_events)
+    nEvents = 0  # Count total events (post-trigger)
+    nNonFid = 0  # Count nonfiducial events
+    ecal_energy_hits = []  # Store ECal energy hits for non-fiducial events
 
-    return {
-        "non_fiducial_ratio": ratio,
-        "uncertainty": uncertainty,
-        "non_fiducial_energy": non_fiducial_energy_total
-    }
+    # Loop over files for this mass
+    for i, filename in tqdm(enumerate(file_list), total=nFiles):
+        if nEvents >= 2e4:
+            break
+        try:
+            with uproot.open(filename, interpretation_executor=executor)['LDMX_Events;6'] as t:
+                if not t.keys():
+                    print(f"FOUND ZOMBIE: {filename}  SKIPPING...", flush=True)
+                    continue
+                key_miss = False
+                for branch in branchList:
+                    if not re.split('/', branch)[0] in t.keys():
+                        key_miss = True
+                        break
+                if key_miss:
+                    print(f"MISSING KEY(S) IN: {filename}  SKIPPING...", flush=True)
+                    continue
 
-# Run the analysis
-result = compute_non_fiducial_ratio(file_path)
-print(result)
+                data = t.arrays(branchList, interpretation_executor=executor)
+
+                # tsp leaves
+                pdgID = data[branchList[0]]
+                x = data[branchList[1]]
+                y = data[branchList[2]]
+                z = data[branchList[3]]
+                px = data[branchList[4]]
+                py = data[branchList[5]]
+                pz = data[branchList[6]]
+
+                # Apply trigger for signal
+                if mass:
+                    tskimmed_data = {}
+                    trig_pass = data[branchList[7]]
+                    for branch in branchList:
+                        tskimmed_data[branch] = data[branch][trig_pass]
+                    pdgID = tskimmed_data[branchList[0]]
+                    x = tskimmed_data[branchList[1]]
+                    y = tskimmed_data[branchList[2]]
+                    z = tskimmed_data[branchList[3]]
+                    px = tskimmed_data[branchList[4]]
+                    py = tskimmed_data[branchList[5]]
+                    pz = tskimmed_data[branchList[6]]
+
+                # Add events to running count
+                nEvents += len(z)
+
+                # Select recoil electron at downstream tsp (maximal forward moving pz electron)
+                e_cut = np.zeros_like(px, dtype=bool).tolist()
+                for i in range(len(px)):
+                    maxPz = 0
+                    e_index = -1
+                    for j in range(len(px[i])):
+                        if pdgID[i][j] == 11 and z[i][j] > 0.17 and z[i][j] < 0.18 and pz[i][j] > maxPz:
+                            maxPz = pz[i][j]
+                            e_index = j
+                    if e_index >= 0:
+                        e_cut[i][e_index] = True
+
+                e_cut = awkward.Array(e_cut)
+                recoilX = pad_array(x[e_cut])
+                recoilY = pad_array(y[e_cut])
+                recoilPx = pad_array(px[e_cut])
+                recoilPy = pad_array(py[e_cut])
+                recoilPz = pad_array(pz[e_cut])
+
+                # Fiducial cut
+                N = len(recoilX)
+                f_cut = np.zeros(N, dtype=bool)
+                for i in range(N):
+                    fiducial = False
+                    fXY = projection(recoilX[i], recoilY[i], SP_TARGET_DOWN_Z, recoilPx[i], recoilPy[i], recoilPz[i], ECAL_FACE_Z)
+                    if not (recoilX[i] == 0 and recoilY[i] == 0 and recoilPx[i] == 0 and recoilPy[i] == 0 and recoilPz[i] == 0):
+                        for j in range(len(cells)):
+                            celldis = dist(cells[j], fXY)
+                            if celldis <= CELL_RADIUS:
+                                fiducial = True
+                                break
+                    if fiducial:
+                        f_cut[i] = True
+
+                # Add nonfiducial count to running total
+                non_fid_events = f_cut == 0
+                nNonFid += np.sum(non_fid_events)
+
+                # Extract ECal energy for non-fiducial events
+                energy = data[branchList[8]]
+                isNoise = data[branchList[9]]
+
+                for event in range(len(energy)):
+                    for hit in range(len(energy[event])):
+                        if not isNoise[event][hit] and non_fid_events[event]:
+                            ecal_energy_hits.append(energy[event][hit])
+
+        except OSError:
+            continue
+
+    # Compute nonfiducial ratio (for this mass point)
+    if nEvents > 0:
+        nonfid_ratio = nNonFid / nEvents
+        nonfid_uncertainty = nonfid_ratio * (math.sqrt(nNonFid) / nNonFid + math.sqrt(nEvents) / nEvents)
+        nonfid_ratios[mass] = {
+            "ratio": nonfid_ratio,
+            "uncertainty": nonfid_uncertainty,
+            "ecal_energy_hits": ecal_energy_hits
+        }
+    else:
+        nonfid_ratios[mass] = "no events"
+
+# Print non-fiducial ratios with uncertainties
+print(json.dumps(nonfid_ratios, indent=4))
