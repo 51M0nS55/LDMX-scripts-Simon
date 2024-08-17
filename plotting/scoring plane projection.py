3e4f1c0692f34a3c919e11c6495f396ccfc1f1f2
Simon S <157428226+51M0nS55@users.noreply.github.com>
2024-08-16 21:57:01 -0700

    Update scoring plane projection.py

--- plotting/scoring plane projection.py
+++ plotting/scoring plane projection.py
@@ -109,7 +109,7 @@ for mass in file_templates.keys():
     # loop over files of this mass
     for i, filename in tqdm(enumerate(file_list), total=nFiles):
         # stop after i events
-        if nEvents >= 100:
+        if nEvents >= 10000:
             break
         try:
             with uproot.open(filename, interpretation_executor=executor) as file:
